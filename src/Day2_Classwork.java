f84750939470769adef5769fa18b037785efcc82
Lotus <cmadisonne@gmail.com>
2018-07-23 11:30:51 -0500

    JavaLoops

---
+++ src/Day2_Classwork.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Day2_Classwork {
+
+    public static void main(String[] args) {
+
+        // Exercise 8 //
+        Scanner in = new Scanner(System.in);
+//        System.out.println("How awesome is Kenn and Kevin");
+//        String userInput = in.nextLine();
+
+//        while (!userInput.equals("very awesome")) {
+//            System.out.println("Try again.");
+//            userInput = in.nextLine();
+//        }
+
+        // Exercise 9 //
+//        String hardCode = "mc98";
+//        System.out.println("Enter password.");
+//        String userGuess = in.nextLine();
+
+//        while (!userGuess.equals(hardCode)) {
+//            System.out.println("Try again.");
+//            userGuess = in.nextLine();
+//        }
+//    }
+
+        // Exercise 10 //
+//        int counter = 0;
+//        while (counter <= 100) {
+//            System.out.println(counter);
+//            counter += 1;
+//        }
+
+        // Exercise 11 //
+        int counter2 = 0;
+        while (counter2 <= 100) {
+            System.out.println(counter2);
+            counter2 += 2;
+        }
+//    }
+
+    }
+}
